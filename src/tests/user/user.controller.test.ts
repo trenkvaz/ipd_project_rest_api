582dbd9ad434bf4036069bf861513fdf5ed183a8
pozdnaykovaa <aapozdnyakov@lanit.ru>
2025-12-22 00:36:03 +0500

    настроен тест юзеров с запуском докера, начат редми

--- src/tests/user/user.controller.test.ts
+++ src/tests/user/user.controller.test.ts
@@ -5,8 +5,51 @@ import {OrderController} from "../../controllers/order.controller";
 import {OrderService} from "../../services/order.service";
 import { describe, it, expect, beforeEach,beforeAll,afterAll } from '@jest/globals';
 import {IOrder} from '../../types/order';
+import axios from 'axios';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+import { exec } from 'child_process';
+import { promisify } from 'util';
+const execPromise = promisify(exec);
+
+
+const startContainers = async () => {
+    try {
+        const { stdout } = await execPromise("docker-compose -f docker-compose_test.yml up -d");
+        console.log(stdout);
+    } catch (error:any) {
+        throw new Error(`Error starting containers: ${error}`);
+    }
+};
+
+const stopContainers = async () => {
+    try {
+        const { stdout } = await execPromise("docker-compose -f docker-compose_test.yml down");
+        console.log(stdout);
+    } catch (error:any) {
+        throw new Error(`Error stopping containers: ${error}`);
+    }
+};
+
+
+const waitForService = async (url: string, timeout: number = 30000) => {
+    const startTime = Date.now();
+    while (true) {
+        try {
+            await axios.get(url);
+            console.log(`Service is up at ${url}`);
+            return;
+        } catch (error:any) {
+            if (Date.now() - startTime > timeout) {
+                throw new Error(`Service did not start in time: ${url}`);
+            }
+            if(error.status&&error.status===403)return;
+            console.log(`Waiting for service at ${url}...${error.status}`);
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Ждем 1 секунду перед повторной попыткой
+        }
+    }
+};
 //let orderService = new OrderService();
 
 /*const app = express();
@@ -33,17 +76,20 @@ beforeAll(async () => {
         application = await App.create();
         app = application.getApp();
     } else {
+        //jest.setTimeout(30000);
+        await startContainers();
+        await waitForService("http://localhost:3100/orders");
         let port:string = process.env.PORT!.toString();
         app = 'http://localhost:'+port;
         console.log("app "+app)
     }
     jest.clearAllMocks();
-});
+},30000);
 
 afterAll(async () => {
-    // Закрытие приложения, если это применимо
     console.log("afterAll")
-    if(!isDocker) await application.closeServer(); // Или другой метод очистки
+    if(!isDocker) await application.closeServer();
+    else await stopContainers();
 });
 
 const user_data = {
